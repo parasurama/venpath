90f79cdf79b5579752c5cef7a600a11e6c98d933
Prasanna Parasurama <pparasurama@gmail.com>
2019-04-08 16:50:23 -0400

    snappy codec

--- sample_scripts/repartition_rdd.py
+++ sample_scripts/repartition_rdd.py
@@ -13,5 +13,5 @@ rdd = sc.textFile("/data/share/venpath/snowball/2016/06/*/*.gz")
 
 rdd = rdd.repartition(rdd.getNumPartitions()*25)
 
-rdd.saveAsTextFile("/data/share/venpath/rdd_repartition",
-                   compressionCodecClass="org.apache.hadoop.io.compress.Snappy")
+rdd.saveAsTextFile("/data/share/venpath/rdd_partition",
+                   compressionCodecClass="org.apache.hadoop.io.compress.SnappyCodec")
